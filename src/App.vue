1a0b6e0ae5a6d7537e2b9dd77dfe6e649902c2a7
kafarzada <kafarov_vurgun@mail.ru>
2023-10-12 10:22:04 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class='todo'>
+        <div>
+            <h3>Новые Задачи:</h3>
+            <TaskList :tasks='holdTasks'/>
+        </div>
+        <div>
+            <h3>Выполняются:</h3>
+            <TaskList :tasks='tasksInProcess' />
+        </div>
+        <div>
+            <h3>Выполнены:</h3>
+            <TaskList :tasks='completedTasks' />
+        </div>
+    </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+import { onMounted, ref, computed } from 'vue';
+import type { Ref } from 'vue';
+import storage from './api/client';
+import { ITask } from './types/types';
+import TaskList from './TaskList.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+const tasks: Ref<Array<ITask>> = ref([]);
+
+onMounted(() => {
+    tasks.value = storage.getAllTasks();
 });
+
+const holdTasks = computed(() => tasks.value.filter((task) => task.status === 'On Hold'));
+const tasksInProcess = computed(() => tasks.value.filter((task) => task.status === 'In Progress'));
+const completedTasks = computed(() => tasks.value.filter((task) => task.status === 'Completed'));
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+.todo {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 20px;
+}
+
+.task-list {
+    list-style: none;
+}
+
+.task {
+    width: 300px;
+    border: 1px solid rgb(146, 146, 146);
+    padding: 15px;
+    min-height: 90px;
+    margin-bottom: 15px;
+    cursor: pointer;
+    border-radius: 10px;
+}
+
+.priority {
+    display: inline-block;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 20px;
+}
+
+.Medium {
+    background-color: yellowgreen;
+    color: white;
+}
+
+.Low {
+    background-color: yellow;
+}
+
+.High {
+    background-color: red;
+    color: white;
 }
 </style>
